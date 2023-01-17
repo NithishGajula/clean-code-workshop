101367250c0ff20346f9844d0754967c00287e47
NithishGajula <nithishg1199@gmail.com>
2023-01-17 11:51:34 +0530

    [Nithish] add characteristic test for customer class

--- src/test/java/com/movierental/CustomerTest.java
+++ src/test/java/com/movierental/CustomerTest.java
@@ -2,8 +2,24 @@ package com.movierental;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class CustomerTest {
     @Test
-    public void test(){}
+    public void test(){
+        Customer customer = new Customer("Nithish");
+        customer.addRental(new Rental(new Movie("RRR", 1), 5));
+        customer.addRental(new Rental(new Movie("Pushpa", 2), 3));
+        customer.addRental(new Rental(new Movie("KGF", 0), 2));
+        customer.addRental(new Rental(new Movie("Kantara", 1), 1));
+
+        assertEquals("Rental Record for Nithish\n" +
+                "\tRRR\t15.0\n" +
+                "\tPushpa\t1.5\n" +
+                "\tKGF\t2.0\n" +
+                "\tKantara\t3.0\n" +
+                "Amount owed is 21.5\n" +
+                "You earned 5 frequent renter points", customer.statement());
+    }
 
 }
