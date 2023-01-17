7e9974506bb75286a731eb43547383b6818baacd
NithishGajula <nithishg1199@gmail.com>
2023-01-17 21:37:59 +0530

    [Nithish] refactor to remove feature envy

--- src/main/java/com/movierental/Customer.java
+++ src/main/java/com/movierental/Customer.java
@@ -24,8 +24,8 @@ public class Customer {
         int frequentRenterPoints = 0;
         StringBuilder result = new StringBuilder("Rental Record for " + getName() + "\n");
         for (Rental each : rentals) {
-          double thisAmount = getAmountForRental(each);
-          // add frequent renter points
+            double thisAmount = each.getAmount();
+            // add frequent renter points
             frequentRenterPoints++;
             // add bonus for a two day new release rental
             if (isTwoDayNewReleaseRental(each))
@@ -42,29 +42,8 @@ public class Customer {
         return result.toString();
     }
 
-  private double getAmountForRental(Rental each) {
-    double thisAmount = 0;
-    //determine amounts for each line
-    switch (each.getMovie().getPriceCode()) {
-        case Movie.REGULAR:
-            thisAmount += 2;
-            if (each.getDaysRented() > 2)
-                thisAmount += (each.getDaysRented() - 2) * 1.5;
-            break;
-        case Movie.NEW_RELEASE:
-            thisAmount += each.getDaysRented() * 3;
-            break;
-        case Movie.CHILDRENS:
-            thisAmount += 1.5;
-            if (each.getDaysRented() > 3)
-                thisAmount += (each.getDaysRented() - 3) * 1.5;
-            break;
+    private boolean isTwoDayNewReleaseRental(Rental rental) {
+        return rental.getMovie().getPriceCode() == Movie.NEW_RELEASE && rental.getDaysRented() > 1;
     }
-    return thisAmount;
-  }
-
-  private boolean isTwoDayNewReleaseRental(Rental rental) {
-    return rental.getMovie().getPriceCode() == Movie.NEW_RELEASE && rental.getDaysRented() > 1;
-  }
 }
 
